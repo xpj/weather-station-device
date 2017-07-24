d2af6dc7659c03188f18f42acbdc134bc63f8df0
xpj <xpj@cetoraz.info>
2017-07-24 17:34:40 +0200

    BME280 lib

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # WemosWeatherStation
 Wemos Weather Station
 
+
+##Libraries:
+
+**Adafruit BMP085 Unified by Adafruit Industries**
+https://github.com/adafruit/Adafruit_BMP085_Unified.git
+
+**Adafruit Unified Sensor by Adafruit**
+https://github.com/adafruit/Adafruit_Sensor
+
+**BH1750 by claws**
+https://github.com/claws/BH1750.git
+
+**BME280 by Tyler Glenn**
+https://www.github.com/finitespace/BME280
+
+**RTClib by Adafruit**
+https://github.com/adafruit/RTClib
+
+**PubSubClient by Nick O'Leary**
+https://github.com/knolleary/pubsubclient.git
+
+**Time by Michael Margolis, Paul Stoffregen**
+http://playground.arduino.cc/Code/Time
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
+#include <SPI.h>
 
 #include <BH1750.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
+#include <BME280I2C.h>
 
 #include "I2CScanner.h"
 #include "RTCAdafruit.h"
@@ -31,6 +33,36 @@ RTCAdafruit *rtclock;
 
 BH1750 lightMeter(0x23);
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+BME280I2C bme280;
+
+typedef enum {
+    TEMP_C = (1),
+    TEMP_F = (2),
+
+} temp_unit;
+
+typedef enum {
+    // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
+    PA = (0),
+    HPA = (1),
+    HG = (2),
+    ATM = (3),
+    BAR = (4),
+    TORR = (5),
+    NM2 = (6),
+    PSI = (7)
+} pressure_unit;
+
+typedef struct {
+    float temperature;
+    temp_unit temperatureUnit;
+
+    float pressure;
+    pressure_unit pressureUnit;
+
+    float relative_humidity;
+} units_t;
+
 
 WiFiClient wifiClient;
 
@@ -114,6 +146,13 @@ void connectLightMeter() {
     Serial.println();
 }
 
+void connectBMP280() {
+    if (!bme280.begin()) {
+        Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+        while (1);
+    }
+}
+
 void connectPressureSensor() {
     Serial.println("Connecting BMP180 Pressure and Temperature Sensor");
     if (!bmp.begin()) {
@@ -133,30 +172,59 @@ uint16_t getLux() {
     return lightMeter.readLightLevel();
 }
 
-sensors_event_t getBMPEvent() {
-    sensors_event_t event;
-    bmp.getEvent(&event);
+void getBMP280Event(units_t *event) {
 
-    return event;
+    uint8_t pressureUnit(HPA);
+
+    float pressure(NAN), temperature(NAN), relative_humidity(NAN);
+    bme280.read(pressure, temperature, relative_humidity, pressureUnit, true);
+
+    memset(event, 0, sizeof(units_t));
+    event->pressure = pressure;
+    event->pressureUnit = HPA;
+    event->temperature = temperature;
+    event->temperatureUnit = TEMP_C;
+    event->relative_humidity = relative_humidity;
 }
 
-float getPressure(sensors_event_t event) {
-    if (event.pressure) {
-        return event.pressure;
-    }
+float getAdaPressure() {
+    float pressure;
+    bmp.getPressure(&pressure);
+
+    return pressure;
 }
 
-float getTemperature(sensors_event_t event) {
+float getAdaTemperature() {
     float temperature;
     bmp.getTemperature(&temperature);
 
     return temperature;
 }
 
-float getAltitude(sensors_event_t event) {
+float getAdaAltitude() {
     float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
-    return bmp.pressureToAltitude(seaLevelPressure, event.pressure);
+    return bmp.pressureToAltitude(seaLevelPressure, getAdaPressure());
 }
+
+
+float getPressure(units_t event) {
+    if (event.pressure) {
+        return event.pressure;
+    }
+}
+
+float getHumidity(units_t event) {
+    if (event.relative_humidity) {
+        return event.relative_humidity;
+    }
+}
+
+float getTemperature(units_t event) {
+    if (event.temperature) {
+        return event.temperature;
+    }
+}
+
 
 void handleRainGaugeTick() {
     unsigned long rainGaugeTickTime = millis();
@@ -233,35 +301,46 @@ void process() {
 
     getRainGaugeTicks();
 
-    sensors_event_t event = getBMPEvent();
+    units_t event280;
+    getBMP280Event(&event280);
 
     Serial.print("Light:       ");
     Serial.print(getLux());
     Serial.println(" lx");
     Serial.print("Pressure:    ");
-    Serial.print(getPressure(event));
+    Serial.print(getAdaPressure());
     Serial.println(" hPa");
     Serial.print("Temperature: ");
-    Serial.print(getTemperature(event));
+    Serial.print(getAdaTemperature());
     Serial.println(" C");
     Serial.print("Altitude:    ");
-    Serial.print(getAltitude(event));
+    Serial.print(getAdaAltitude());
     Serial.println(" m");
+
+    Serial.print("Press280:    ");
+    Serial.print(getPressure(event280));
+    Serial.println(" hPa");
+    Serial.print("Temp 280:    ");
+    Serial.print(getTemperature(event280));
+    Serial.println(" C");
+    Serial.print("Hum 280:     ");
+    Serial.print(getHumidity(event280));
+    Serial.println(" %");
 
     // Serial.print("Rain:        "); Serial.print(getRainSensorValue()); Serial.println(" %");
 
-    Serial.print("ADC #0:      ");
-    Serial.print(getADC(PCF8591_ADC_CH0));
-    Serial.println(" ");
-    Serial.print("ADC #1:      ");
-    Serial.print(getADC(PCF8591_ADC_CH1));
-    Serial.println(" ");
-    Serial.print("ADC #2:      ");
-    Serial.print(getADC(PCF8591_ADC_CH2));
-    Serial.println(" ");
-    Serial.print("ADC #3:      ");
-    Serial.print(getADC(PCF8591_ADC_CH3));
-    Serial.println(" ");
+//    Serial.print("ADC #0:      ");
+//    Serial.print(getADC(PCF8591_ADC_CH0));
+//    Serial.println(" ");
+//    Serial.print("ADC #1:      ");
+//    Serial.print(getADC(PCF8591_ADC_CH1));
+//    Serial.println(" ");
+//    Serial.print("ADC #2:      ");
+//    Serial.print(getADC(PCF8591_ADC_CH2));
+//    Serial.println(" ");
+//    Serial.print("ADC #3:      ");
+//    Serial.print(getADC(PCF8591_ADC_CH3));
+//    Serial.println(" ");
 
     // sendMQTTTemp(getTemperature(event), getLux(), getPressure(event), getRainSensorValue());
 
@@ -277,6 +356,7 @@ void setup() {
     connectMQTT();
     connectLightMeter();
     connectPressureSensor();
+    connectBMP280();
     connectRainGauge();
 }
 
